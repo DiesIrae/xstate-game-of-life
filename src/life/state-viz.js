70d5ab6af38b1aa4083f92afebaf34ac8d73d68f
Benoit Richert <github@natura-rerum.com>
2019-03-05 23:04:17 +0100

    add state viz

---
+++ src/life/state-viz.js
@@ -0,0 +1,27 @@
+/**
+ 1- Complete this
+ 2- Copy-paste in https://statecharts.github.io/xstate-viz/
+ **/
+
+
+const { actions } = XState
+const { send, assign, raise, log, sendParent } = actions
+
+const initialBoard = [[0, 0]]
+
+
+const getCellNextState = (x, y) => {}
+const updateCell = () => {}
+const updateCounter = () => {}
+
+
+/** Copy code from life.js below (following beacons in code)
+ * + make modifications
+ * Replace return of getAllCells by this
+ ```
+ [boardCells[0][0].id]: boardCells[0][0],
+ [boardCells[0][1].id]: boardCells[0][1],
+ ```
+ *
+ */
+
